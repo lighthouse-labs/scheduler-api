a13f7156fbaef80cd2250eafa7cf96f7276043f3
Karl Jensen <karljensen@gmail.com>
2019-08-28 21:48:19 -0700

    Change ports, make database reset easier
    - Update instructions to include PG config for running vagrant server
    - Using 8001 by default now
    - Only reset db with GET Route.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8001;
 const ENV = require("./environment");
 
 const app = require("./application")(ENV, { updateAppointment });
