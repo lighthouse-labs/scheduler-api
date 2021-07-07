3b83c0e690652455cd1ab55a674fa1cb3a0eaa5a
Francis Bourgouin <11548647+FrancisBourgouin@users.noreply.github.com>
2021-07-07 15:26:01 -0400

    Fix - SSL issue on local development

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,7 @@ const pg = require("pg");
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 client
